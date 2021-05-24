db1e870b3d4287e5bd277b216bced661581e87ff
Caj Morgan <caj@cajmorgan.com>
2021-05-24 22:56:16 +0200

    functional version

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ const grid = document.querySelector('.grid');
 let scoreO = 0;
 let scoreX = 0;
 
+function onLoadAni() {
+    let i = 100;
+    let looper = setInterval(() => {
+        i--;
+        document.querySelector('.X').textContent = '' + i;
+        document.querySelector('.O').textContent = '' + i;
+        if(i == 0) {
+            clearInterval(looper);
+        }
+    })
+    
+}
+onLoadAni();
+
 function playGame(depth = -1, startingPlayer = 1) {
     startBtn.disabled = true;
     let p = new Player(parseInt(depth));
@@ -17,6 +31,7 @@ function playGame(depth = -1, startingPlayer = 1) {
         for(let i = 0; i <= allCells.length; i++) {
             if(allCells[i]) {
                 allCells[i].textContent = '';
+                allCells[i].style.backgroundColor = "transparent";
             } else if (!allCells[i]) {
                 continue;
             }
@@ -49,14 +64,19 @@ function playGame(depth = -1, startingPlayer = 1) {
             } 
             if(board.isTerminal().winner == 'x') {
                 scoreX += 1;
+                document.querySelector('.X').textContent = scoreX
                 console.log(scoreX);
+                //Run color explosion
+
             }
             let cellId = e.target.id; 
             board.currentState[cellId.slice(-1)] = 'x';
             e.target.textContent = 'x';
+            e.target.style.backgroundColor = "lightblue";
             board.printConsoleBoard();
             console.log(board.isTerminal()['winner']);
             if (board.isTerminal().winner == 'draw') {
+                startBtn.textContent = "Play Again";
                 startBtn.disabled = false; 
                 return;
             }
@@ -72,13 +92,64 @@ function playGame(depth = -1, startingPlayer = 1) {
             board.currentState[computerSelection] = 'o';
             const divSelection = document.querySelector(`#cell${computerSelection}`);
             divSelection.textContent = 'o';
+            divSelection.style.backgroundColor = "red";
             board.printConsoleBoard();
             console.log(board.isTerminal().winner);
             if(board.isTerminal().winner == 'o') {
                 scoreO += 1;
+                document.querySelector('.O').textContent = scoreO
                 console.log(scoreO);
                 startBtn.textContent = "Play Again";
                 startBtn.disabled = false;
+                //Blinking colors 
+                const allCellss = document.querySelectorAll('.cell')
+                const winCombos = [allCellss[0].textContent + allCellss[1].textContent + allCellss[2].textContent, 
+                                    allCellss[3].textContent + allCellss[4].textContent + allCellss[5].textContent,
+                                    allCellss[6].textContent + allCellss[7].textContent + allCellss[8].textContent,
+                                    allCellss[0].textContent  + allCellss[3].textContent + allCellss[6].textContent,
+                                    allCellss[1].textContent + allCellss[4].textContent + allCellss[7].textContent,
+                                    allCellss[2].textContent + allCellss[5].textContent + allCellss[8].textContent ,
+                                    allCellss[0].textContent + allCellss[4].textContent + allCellss[8].textContent,
+                                    allCellss[2].textContent + allCellss[4].textContent + allCellss[6].textContent];
+                const winCombosBreak = [allCellss[0], allCellss[1], allCellss[2], 
+                                        allCellss[3], allCellss[4], allCellss[5],
+                                        allCellss[6], allCellss[7], allCellss[8],
+                                        allCellss[0], allCellss[3], allCellss[6],
+                                        allCellss[1], allCellss[4], allCellss[7],
+                                        allCellss[2], allCellss[5], allCellss[8],
+                                        allCellss[0], allCellss[4], allCellss[8],
+                                        allCellss[2], allCellss[4], allCellss[6]];
+                                    // let filtered = winCombos.forEach(function(item, index) {
+                                    //     if(item === 'ooo') {
+                                    //         let numIt = index
+                                    //         return numIt;
+                                    //     }
+                                    // });
+                                    let findIt = (item) => item === 'ooo';  
+                                    let filtered = winCombos.findIndex(findIt);
+                                        let color = 'orange';
+                                        function colorAlgorithm() {
+                                                filtered = (filtered + 1) * 3;
+                                                let i = 0;
+                                            let colorLoop = setInterval(() => {
+                                                i += 1;
+                                                winCombosBreak[filtered - 1].style.backgroundColor = color;
+                                                winCombosBreak[filtered - 2].style.backgroundColor = color;
+                                                winCombosBreak[filtered - 3].style.backgroundColor = color;
+                                                if(i % 2 == 0) {
+                                                    color = 'orange';
+                                                } else {
+                                                    color = 'green';
+                                                }
+                                                startBtn.addEventListener('click', () => {
+                                                    clearInterval(colorLoop);
+                                                })
+                                            }, 300) 
+                                            
+                                            
+                                        }
+                                        colorAlgorithm();
+                                    console.log(filtered);
                 return;
             }
             if(board.isTerminal() != false) {
